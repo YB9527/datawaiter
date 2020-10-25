9c7f0dce07aa1299b45985512b34fb3bb435c49e
Administrator <525730167@qq.com>
2020-10-25 10:27:08 +0800

    model 初定

--- src/main/java/cn/yb/datawaiter/model/LevelManager.java
+++ src/main/java/cn/yb/datawaiter/model/LevelManager.java
@@ -1,7 +1,27 @@
 package cn.yb.datawaiter.model;
 
+import lombok.Data;
+
 /**
  * 层级管理
  */
+@Data
 public class LevelManager {
+    private  String id;
+    /**
+     * 简单名字
+     */
+    private  String simpleName;
+    /**
+     * 全名称
+     */
+    private  String fullName;
+    /**
+     * 描述
+     */
+    private  String describe;
+    /**
+     * 父节点id
+     */
+    private  String parentId;
 }
